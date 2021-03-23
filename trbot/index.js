42b40a9f2a17e1547d74807443b8e87e52529e93
이은찬 <chan@Chan-PRO.local>
2021-03-24 00:27:47 +0900

    프로젝트 생성

---
+++ trbot/index.js
@@ -0,0 +1,24 @@
+const request = require('request')
+const uuidv4 = require("uuid/v4")
+const sign = require('jsonwebtoken').sign
+
+const {access_key, secret_key} = process.env;
+const server_url = 'https://api.upbit.com'
+
+const payload = {
+    access_key: access_key,
+    nonce: uuidv4(),
+}
+
+const token = sign(payload, secret_key)
+
+const options = {
+    method: "GET",
+    url: server_url + "/v1/accounts",
+    headers: {Authorization: `Bearer ${token}`},
+}
+
+request(options, (error, response, body) => {
+    if (error) throw new Error(error)
+    console.log(body)
+})
